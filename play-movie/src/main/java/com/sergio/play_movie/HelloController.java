8bbfe5d9cac2d91129f96c4d40f258b556d31922
Sergio Guzmán <serguz95444@gmail.com>
2025-11-26 15:24:34 -0600

    Se agregaron variables de configuracion y actualizacion al servicio PlayAI

--- play-movie/src/main/java/com/sergio/play_movie/HelloController.java
+++ play-movie/src/main/java/com/sergio/play_movie/HelloController.java
@@ -1,19 +1,23 @@
 package com.sergio.play_movie;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final String platform;
+
     private final PlayAiService aiService;
 
-    public HelloController(PlayAiService aiService) {
+    public HelloController(@Value("${spring.application.name}") String platform, PlayAiService aiService) {
+        this.platform = platform;
         this.aiService = aiService;
     }
 
     @GetMapping("/")
     public String hello(){
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(platform);
     }
 }
--- play-movie/src/main/java/com/sergio/play_movie/PlayAiService.java
+++ play-movie/src/main/java/com/sergio/play_movie/PlayAiService.java
@@ -1,14 +1,15 @@
 package com.sergio.play_movie;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Gestión de Películas Play Movie.
+            Genera un saludo de bienvenida a la plataforma de Gestión de Películas {{plataform}}.
             Usa menos de 120 caracteres y hazlo con el estilo de Netflix.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("plataform")String plataform);
 }
