c75e4aedd6109c076153b9150e2dcf60f78d1a3b
Sergio Guzmán <serguz95444@gmail.com>
2025-11-30 20:34:51 -0600

    Agrega validaciones a los campos de MovieDto para asegurar la integridad de los datos

--- play-movie/src/main/java/com/sergio/play_movie/domain/dto/MovieDto.java
+++ play-movie/src/main/java/com/sergio/play_movie/domain/dto/MovieDto.java
@@ -4,13 +4,25 @@ import java.time.LocalDate;
 
 import com.sergio.play_movie.domain.Genre;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+
 public record MovieDto(
 		Long id,
-		String title,
-		Integer duration,
+
+		@NotBlank(message = "El título no puede estar vacío") String title,
+
+		@Positive(message = "La duración debe ser un número positivo") Integer duration,
+
 		Genre genre,
-		LocalDate realeaseDate,
-		Double rating,
+
+		@Past(message = "La fecha de lanzamiento debe ser una fecha pasada") LocalDate realeaseDate,
+
+		@Min(value = 0, message = "La calificación debe ser mayor a 0") @Max(value = 10, message = "La calificación debe ser menor a 10") Double rating,
+
 		Boolean state) {
 
 }
